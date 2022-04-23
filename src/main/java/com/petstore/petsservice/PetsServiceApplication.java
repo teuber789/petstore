1cc4ed849f617f817dc39496ad0570101245d3ab
Joseph Cotton <catanjunkie789@gmail.com>
2022-04-23 13:10:39 -0400

    Initial commit

---
+++ src/main/java/com/petstore/petsservice/PetsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.petstore.petsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsServiceApplication.class, args);
+	}
+
+}
